7cbbdfefac9fc6a678bd15bc6b45065eb64fb76f
Flevertyapp <Flevertyap@gmail.com>
2022-03-22 23:50:07 +0300

    Practice

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -16,5 +16,21 @@ public class Main {
         //труъ создавать в одну строку- Cat cat3 = new Cat()
 
         Cat cat4=new Cat("Izya", "Purple", 7);
+
+        //practice
+        Staff[] staffArray= new Staff[6];
+        staffArray[0]=new Staff("Vasya", "cleaner", "asd@dsa.uk", "89456987451", 25000, 54);
+        staffArray[1]=new Staff("Petya", "engineer", "asd@dsa.uk", "89456987451", 30000, 40);
+        staffArray[2]=new Staff("Petrucho", "student", "asd@dsa.uk", "89456987451", 35000, 27);
+        staffArray[3]=new Staff("Ezhi", "engineer", "asd@dsa.uk", "89456987451", 40000, 65);
+        staffArray[4]=new Staff("Vasisuali", "talisman", "asd@dsa.uk", "89456987451", 50000, 35);
+        staffArray[5]=new Staff("Serafim", "SEO", "asd@dsa.uk", "89456987451", 55000, 21);
+        //staffArray[5].staffInfo();
+
+        for (int i = 0; i <staffArray.length ; i++) {
+            if(staffArray[i].getAge()>=40 ){
+                staffArray[i].staffInfo();
+            }
+        }
     }
 }
